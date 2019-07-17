f4d119ed04201f7b951781c2061f6c3960699f87
pretzeldknight <s.velugula@student.saedubai.com>
2019-07-17 22:17:38 +0400

    Added Basic Menu BG and Visualizer display. Also added in new mesh for the Particle waves

---
+++ Assets/Scripts/Aesthetic scripts/AudioVisualizer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualizer : MonoBehaviour
+{
+    [Range(1.0f, 4500.0f)]
+    public float multiplier;
+    public int minRange = 0;
+    public int maxRange = 64;
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    private float prevAvg = 0.0f;
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * 0.05f;
+
+        float[] spectrum = new float[64];
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        if (maxRange < minRange)
+            maxRange = minRange + 1;
+
+        minRange = Mathf.Clamp(minRange, 0, 63);
+        maxRange = Mathf.Clamp(maxRange, 0, 63);
+
+        float avg = 0;
+
+        for (int i = minRange; i < maxRange; i++)
+            avg += Mathf.Abs(spectrum[i]);
+
+        avg = avg / (float)Mathf.Abs(maxRange - minRange);
+
+        if (avg - prevAvg > 0.0012f) // avg is > than prevAvg
+        {
+            avg = prevAvg + 0.0012f;
+            time = 0;
+        }
+        else if (avg - prevAvg < -0.0012f) // avg is < than prevAvg
+        {
+            avg = prevAvg - 0.0012f;
+            avg = Mathf.Lerp(prevAvg, prevAvg - 0.001f, time);
+        }
+
+        skinnedMeshRenderer.SetBlendShapeWeight(0, avg * multiplier);
+        prevAvg = avg;
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum MenuState { Main, NewGame, Continue, Options }
+
+public class MenuManager : MonoBehaviour
+{
+    MenuState state;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = MenuState.Main;
+        anim.SetBool("Main", true);
+        anim.SetBool("NewGame", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case MenuState.Main:
+                anim.SetBool("NewGame", false);
+                anim.SetBool("Main", true);
+                break;
+            case MenuState.NewGame:
+                anim.SetBool("NewGame", true);
+                anim.SetBool("Main", false);
+                break;
+            case MenuState.Continue:
+                anim.SetBool("NewGame", false);
+                anim.SetBool("Main", false);
+                break;
+            case MenuState.Options:
+                anim.SetBool("NewGame", false);
+                anim.SetBool("Main", false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void NewGame()
+    {
+        state = MenuState.NewGame;
+    }
+
+    public void Continue()
+    {
+        state = MenuState.Continue;
+    }
+
+    public void Options()
+    {
+        state = MenuState.Options;
+    }
+
+    public void Main()
+    {
+        state = MenuState.Main;
+    }
+}
