cebde4b6fd4e7a1e7ffc63232bedeb25e5aba3c6
pretzeldknight <s.velugula@student.saedubai.com>
2019-07-20 11:59:45 +0400

    Added an Opening animation leading into the Menu screen along with a fade in and a Idle where the Logo brightens and darkens

--- Assets/Scripts/Aesthetic scripts/CursorScript.cs
+++ Assets/Scripts/Aesthetic scripts/CursorScript.cs
@@ -10,12 +10,9 @@ public class CursorScript : MonoBehaviour
 
     List<ParticleSystem> ripples;
 
-    float time;
-
     // Start is called before the first frame update
     void Start()
     {
-        time = 0;
         Cursor.visible = false;
         ripples = new List<ParticleSystem>();
     }
@@ -23,8 +20,6 @@ public class CursorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-
         Vector3 screenPoint = Input.mousePosition;
         screenPoint.z = canvas.planeDistance - 0.03f; //distance of the plane from the camera
         transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
@@ -32,27 +27,9 @@ public class CursorScript : MonoBehaviour
         if (Input.GetKey(KeyCode.Mouse0))
         {
             ParticleSystem temp = Instantiate(ripple);
+            temp.tag = "Ripple";
             ripples.Add(Instantiate(temp));
             temp.transform.position = transform.position;
-            temp.tag = "Ripple";
-            temp.Play();
-            Destroy(temp.gameObject, 1f);
-            time = 0;
-        }
-        else
-        {
-            if (time >= 1f)
-            {
-                DestroyRipples();
-            }
-        }
-    }
-
-    void DestroyRipples()
-    {
-        for (int i = 0; i < ripples.Count; i++)
-        {
-            Destroy(ripples[i]);
         }
     }
 }
---
+++ Assets/Scripts/Aesthetic scripts/ParticleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    float time;
+
+    ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        if (gameObject.tag == "Ripple")
+            particle.Play();
+        //Destroy(particle.gameObject, 1f);
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.tag == "Ripple")
+            time += Time.deltaTime;
+
+        if (time >=1f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
