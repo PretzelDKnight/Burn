76a92274a83cebb305a2c6da512402f9ee488238
pretzeldknight <s.velugula@student.saedubai.com>
2019-07-25 19:58:14 +0400

    Converted lights to baking

---
+++ Assets/Scripts/InProgress/LevelCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCam : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if (move != 0)
+        {
+            transform.position += new Vector3(move,0,0) * speed * Time.deltaTime;
+        }
+    }
+}
