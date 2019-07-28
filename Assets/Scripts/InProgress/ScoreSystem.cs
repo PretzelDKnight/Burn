7fbeb7be84e0998bd9aa8e5c4b53d91ca3781ce3
Kyle Fernandes <12rmaker@gmail.com>
2019-07-29 02:58:47 +0400

    Made changes to scoring system, mad eit so that there is a fixed score that decreases within a time limit, thought score decrements very quickly

--- Assets/Scripts/InProgress/ScoreSystem.cs
+++ Assets/Scripts/InProgress/ScoreSystem.cs
@@ -9,6 +9,7 @@ public class ScoreSystem : MonoBehaviour
     public delegate void scoresystem();
     public delegate void combomockup(int i);
     public static event scoresystem timescore;
+    public static event scoresystem countdownscore;
     public static event combomockup combomock;
     int i = 0;
 
@@ -24,6 +25,8 @@ public class ScoreSystem : MonoBehaviour
         if (timescore != null)
             timescore();
 
+        if (countdownscore != null)
+            countdownscore();
 
         if (Input.GetKeyDown(KeyCode.X))
         {
--- Assets/Scripts/InProgress/score.cs
+++ Assets/Scripts/InProgress/score.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class score : MonoBehaviour
 {
     float pscore = 0;
+    float countdown = 300;
+    float levelscore = 3000;
+
     // Start is called before the first frame update
     void Start()
     {
         ScoreSystem.timescore += timedscore;
         ScoreSystem.combomock += comboscore;
+        ScoreSystem.countdownscore += countdownscores;
     }
 
     // Update is called once per frame
     void Update()
     {
-        pscore += Time.deltaTime;
+        //pscore += Time.deltaTime;
     }
 
     void timedscore()
@@ -32,5 +36,17 @@ public class score : MonoBehaviour
             pscore = pscore * i;
 
         Debug.Log("i has been pressed" + i + "times");
+    }
+
+    void countdownscores()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown != 0)
+        {
+            levelscore -= 10;
+        }
+
+        Debug.Log(levelscore);
     }
 }
