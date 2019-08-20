b1803d64f8aa2ca575f82077831d76f292d5d068
Blank <a.gondesi@student.saedubai.com>
2019-08-21 00:23:45 +0400

    Added save the set keybinds to player prefs so that the changed keybinds will load when the game is closed and opened again.

--- Assets/Scripts/InProgress/KeyBindings.cs
+++ Assets/Scripts/InProgress/KeyBindings.cs
@@ -5,21 +5,23 @@ using UnityEngine.UI;
 
 public class KeyBindings : MonoBehaviour
 {
-    public Dictionary<string, KeyCode> inputs = new Dictionary<string, KeyCode>();
-    public Text Jump, Attack, Interact, Dash, Forward, Backwards;
-    [SerializeField] GameObject ChangeInstructionPannel;
-    GameObject currentKey;
+    public Dictionary<string, KeyCode> inputs = new Dictionary<string, KeyCode>();          //Creating dictionary of the input keys
+    public Text Jump, Attack, Interact, Dash, Forward, Backwards;                           //The texts shown inside the button (the key binding is shown)
+    [SerializeField] GameObject ChangeInstructionPannel;                                    //The instruction pannel
+    GameObject currentKey;                                                                  //The button which is selected to change the keybind off
 
     // Start is called before the first frame update
     void Start()
     {
-        inputs.Add("Jump", KeyCode.Z);
-        inputs.Add("Attack", KeyCode.X);
-        inputs.Add("Interact", KeyCode.A);
-        inputs.Add("Dash", KeyCode.C);
-        inputs.Add("Forward", KeyCode.RightArrow);
-        inputs.Add("Backward", KeyCode.LeftArrow);
+        //Adding the lable in the dictionary and its meaaning(what it will return)
+        inputs.Add("Jump",(KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Z")));
+        inputs.Add("Attack",(KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Attack", "X")));
+        inputs.Add("Interact",(KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "A")));
+        inputs.Add("Dash", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Dash", "C")));
+        inputs.Add("Forward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward", "RightArrow")));
+        inputs.Add("Backward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Backward", "LeftArrow")));
 
+        //Setting the text in the buttons to show the key name of that keybinding
         Jump.text = inputs["Jump"].ToString();
         Attack.text = inputs["Attack"].ToString();
         Interact.text = inputs["Interact"].ToString();
@@ -30,6 +32,7 @@ public class KeyBindings : MonoBehaviour
 
     private void OnGUI()
     {
+        //Checking if the button is click if so then changing the keybinding
         if (currentKey != null)
         {
             Event e = Event.current;
@@ -43,15 +46,22 @@ public class KeyBindings : MonoBehaviour
         }
     }
 
+
+    //Function that will set the current key to have a value to cahnge too and shows instruction pannal
     public void clickedToChange(GameObject thisIsClicked)
     {
         currentKey = thisIsClicked;
         ChangeInstructionPannel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Function to save the changes in keyBindings
+    public void SaveKeyBindings()
     {
-        
+        //looping through the current set keybindings and aving playerPrefs
+        foreach(var key in inputs)
+        {
+            PlayerPrefs.SetString(key.Key, key.Value.ToString());
+        }
+        PlayerPrefs.Save();
     }
 }
