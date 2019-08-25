6493657cac4fed4b8af5805906b79b4a217b03f0
Kyle Fernandes <12rmaker@gmail.com>
2019-08-26 03:50:08 +0400

    Enemy AI added along with several changes, decided to use navmesh instead

---
+++ Assets/Scripts/InProgress/EnemyAI.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemyAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] float decisionDelay = 3f;
+    [SerializeField] Transform objectToChase;
+    [SerializeField] Transform[] destinations;
+    int currentDes = 0;
+    public GameObject Enemy1;
+    public int EnemyHealth = 10;
+
+    enum EnemyStates
+    {
+        Patrolling,
+        Chasing
+    }
+
+    [SerializeField] EnemyStates currentState;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        InvokeRepeating("SetDestination", 0.5f, decisionDelay);
+        if (currentState == EnemyStates.Patrolling)
+            agent.SetDestination(destinations[currentDes].position);
+    }
+
+    void Update()
+    {
+        Movement();
+
+        if (EnemyHealth <= 0)
+        {
+            Destroy(Enemy1);
+        }
+    }
+
+    void SetDestination()
+    {
+        if (currentState == EnemyStates.Chasing)
+            agent.SetDestination(objectToChase.position);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Hit player");
+        }
+    }
+
+    public void DeductHealth(int DamageAmount)
+    {
+        if (Enemy1)
+        {
+            EnemyHealth -= DamageAmount;
+        }
+    }
+
+    void Movement()
+    {
+        if (Vector3.Distance(transform.position, objectToChase.position) > 10f)
+        {
+            currentState = EnemyStates.Patrolling;
+        }
+        else
+        {
+            currentState = EnemyStates.Chasing;
+        }
+        if (currentState == EnemyStates.Patrolling)
+        {
+            if (Vector3.Distance(transform.position, destinations[currentDes].position) <= 0.6f)
+            {
+                currentDes++;
+                if (currentDes == destinations.Length)
+                {
+                    currentDes = 0;
+                }
+            }
+            agent.SetDestination(destinations[currentDes].position);
+        }
+    }
+}
