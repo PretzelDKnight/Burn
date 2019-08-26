160267a2b69374c211e9a9a30f290447af5ec453
Blank <a.gondesi@student.saedubai.com>
2019-08-26 23:57:21 +0400

    Colliders in level 1, double jump in mall and no elevator. fixed the cars, need to do camera stuff

---
+++ Assets/Scripts/InProgress/CamFollowPlayerR.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayerR : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+
+    //Valuses of the postion of the camera needed to be added to the player postion on start
+    [SerializeField] Vector3 camOffSet;
+
+
+    // Updating the positon of camera to follow the player at all times
+    void LateUpdate()
+    {
+        Vector3 requiredSpot = target.transform.position + camOffSet;
+        transform.position = requiredSpot;
+
+        //transform.LookAt(target.transform);
+    }
+}
